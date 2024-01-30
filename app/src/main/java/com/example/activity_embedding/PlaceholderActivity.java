3dc647dcfb9a26e131319f735ce02e1cc505107d
马暄炜 <maxuanwei@picc.com.cn>
2024-01-30 14:52:53 +0800

    init

---
+++ app/src/main/java/com/example/activity_embedding/PlaceholderActivity.java
@@ -0,0 +1,14 @@
+package com.example.activity_embedding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlaceholderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_placeholder);
+    }
+}
---
+++ app/src/main/java/com/example/activity_embedding/SampleApplication.java
@@ -0,0 +1,23 @@
+package com.example.activity_embedding;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.window.embedding.RuleController;
+import androidx.window.embedding.SplitController;
+
+public class SampleApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (SplitController.getInstance(this).getSplitSupportStatus() ==
+                SplitController.SplitSupportStatus.SPLIT_AVAILABLE) {
+            // Device supports split activity features.
+            Log.e("mxw","SPLIT_AVAILABLE");
+        }else {
+            Log.e("mxw","SPLIT_UNAVAILABLE");
+        }
+        RuleController.getInstance(this)
+                .setRules(RuleController.parseRules(this, R.xml.main_split_config));
+    }
+}
